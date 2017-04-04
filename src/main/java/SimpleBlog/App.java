445e475449a78d566b334f799f8fa2cda6c07bb2
Hao Liu <hao.liu@planplus.com>
2017-04-03 22:32:30 -0400

    Add support of SendGrid API.

--- src/main/java/SimpleBlog/App.java
+++ src/main/java/SimpleBlog/App.java
@@ -1,95 +1,115 @@
 package SimpleBlog;
 
-import SimpleBlog.entity.Blog;
-import SimpleBlog.plugin.*;
-import org.dom4j.io.XMLWriter;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import SimpleBlog.entity.Blog;
+import SimpleBlog.plugin.*;
 
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) throws IOException {
-        ApplicationContext context;
-        context = new ClassPathXmlApplicationContext("spring.xml");
-        String arg = args.length > 0 ? args[0] : "";
-        switch (arg) {
-            case "tumblr":
-                tumblrImport(context);
-                break;
-            case "new":
-                newExportFile(context);
-                break;
-            case "mail":
-                sendMail(context);
-                break;
-            default:
-                System.out.println("java -cp Blog-1.0-SNAPSHOT.jar SimpleBlog.App [ new | tumblr ]");
-                break;
-        }
 
-/*
-        try {
-            byte[] c = importTumblr.fetchRemoteFile("file:///d|/birthday.png");
-            importTumblr.calculateResourceHash(c);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-*/
-    }
+	public static void main(String[] args) throws IOException {
+		ApplicationContext context;
+		context = new ClassPathXmlApplicationContext("spring.xml");
+		String arg = args.length > 0 ? args[0] : "";
+		switch (arg) {
+		case "tumblr":
+			tumblrImport(context);
+			break;
+		case "new":
+			newExportFile(context);
+			break;
+		case "mail":
+			sendMail(context);
+			break;
+		case "mailAPI":
+			sendMailAPI(context);
+			break;
+		default:
+			System.out.println("java -cp Blog-1.0-SNAPSHOT.jar SimpleBlog.App [ new | tumblr ]");
+			break;
+		}
 
-    private static Blog newNote(WeatherData data, DateUtil date) {
-        Blog blog = new Blog();
-        blog.setAuthor("Hao Liu");
-        blog.setSubject(date.getTextDate() + " " + data.getWeather() + " " + data.getLocation());
-        blog.setContent("What a wonderful day. 美好的一天。");
-        blog.setCreate(date.getEvernoteDate());
-        blog.setUpdate(date.getEvernoteDate());
-        blog.setTags("@2017 Diary");
-        return blog;
-    }
+		/*
+		 * try {
+		 * byte[] c = importTumblr.fetchRemoteFile("file:///d|/birthday.png");
+		 * importTumblr.calculateResourceHash(c);
+		 * } catch (Exception e) {
+		 * e.printStackTrace();
+		 * }
+		 */
+	}
 
-    private static void sendMail(ApplicationContext context) {
-        ConvertToEvernote cte = (ConvertToEvernote) context.getBean("convertToEvernote");
-        //YahooWeatherData data = (YahooWeatherData) context.getBean("yahooData");
-        OpenWeatherMapData data = (OpenWeatherMapData) context.getBean("openweathermapData");
-        DateUtil date = (DateUtil) context.getBean("dateUtil");
-        MailToEvernote mailToEvernote = (MailToEvernote) context.getBean("mailToEvernote");
-        Blog blog = newNote(data, date);
+	private static Blog newNote(WeatherData data, DateUtil date) {
+		Blog blog = new Blog();
+		blog.setAuthor("Hao Liu");
+		blog.setSubject(date.getTextDate() + " " + data.getWeather() + " " + data.getLocation());
+		blog.setContent("What a wonderful day. 美好的一天。");
+		blog.setCreate(date.getEvernoteDate());
+		blog.setUpdate(date.getEvernoteDate());
+		blog.setTags("@2017 Diary");
+		return blog;
+	}
 
-        mailToEvernote.send(blog);
-    }
+	private static void sendMail(ApplicationContext context) {
+		ConvertToEvernote cte = (ConvertToEvernote) context.getBean("convertToEvernote");
+		// YahooWeatherData data = (YahooWeatherData)
+		// context.getBean("yahooData");
+		OpenWeatherMapData data = (OpenWeatherMapData) context.getBean("openweathermapData");
+		DateUtil date = (DateUtil) context.getBean("dateUtil");
+		MailToEvernote mailToEvernote = (MailToEvernote) context.getBean("mailToEvernote");
+		Blog blog = newNote(data, date);
 
-    private static void newExportFile(ApplicationContext context) {
-        ConvertToEvernote cte = (ConvertToEvernote) context.getBean("convertToEvernote");
-        //YahooWeatherData data = (YahooWeatherData) context.getBean("yahooData");
-        OpenWeatherMapData data = (OpenWeatherMapData) context.getBean("openweathermapData");
-        DateUtil date = (DateUtil) context.getBean("dateUtil");
+		mailToEvernote.send(blog);
+	}
 
-        Blog blog = newNote(data, date);
-        List<Blog> blogs = new ArrayList<Blog>();
-        blogs.add(blog);
+	private static void sendMailAPI(ApplicationContext context) {
+		ConvertToEvernote cte = (ConvertToEvernote) context.getBean("convertToEvernote");
+		// YahooWeatherData data = (YahooWeatherData)
+		// context.getBean("yahooData");
+		OpenWeatherMapData data = (OpenWeatherMapData) context.getBean("openweathermapData");
+		DateUtil date = (DateUtil) context.getBean("dateUtil");
+		MailToEvernote mailToEvernote = (MailToEvernote) context.getBean("mailToEvernote");
+		Blog blog = newNote(data, date);
 
-        if (new File("./NewExport.enex").exists()) {
-            cte.updateEnex(blogs, "NewExport.enex");
-        } else {
-            cte.createEnex(blogs, "NewExport.enex");
-        }
-    }
+		mailToEvernote.sendByAPI(blog);
+	}
 
-    private static void tumblrImport(ApplicationContext context) {
-        ConvertToEvernote cte = (ConvertToEvernote) context.getBean("convertToEvernote");
-        ImportTumblrPostData importTumblr = (ImportTumblrPostData) context.getBean("importTumblrPostData");
-        String tumblrUrl = "http://liuhao2012.tumblr.com/page/";
-        for (int i = 40; i < 41; i++) {
-            System.out.println(i);
-            cte.updateEnex(importTumblr.getXmlDocument(tumblrUrl + String.valueOf(i)),
-                    "TumblrPostExport.enex");
-        }
-    }
+	private static void newExportFile(ApplicationContext context) {
+		ConvertToEvernote cte = (ConvertToEvernote) context.getBean("convertToEvernote");
+		// YahooWeatherData data = (YahooWeatherData)
+		// context.getBean("yahooData");
+		OpenWeatherMapData data = (OpenWeatherMapData) context.getBean("openweathermapData");
+		DateUtil date = (DateUtil) context.getBean("dateUtil");
+
+		Blog blog = newNote(data, date);
+		List<Blog> blogs = new ArrayList<Blog>();
+		blogs.add(blog);
+
+		if (new File("./NewExport.enex").exists()) {
+			cte.updateEnex(blogs, "NewExport.enex");
+		} else {
+			cte.createEnex(blogs, "NewExport.enex");
+		}
+	}
+
+	private static void tumblrImport(ApplicationContext context) {
+		ConvertToEvernote cte = (ConvertToEvernote) context.getBean("convertToEvernote");
+		ImportTumblrPostData importTumblr = (ImportTumblrPostData) context
+			.getBean("importTumblrPostData");
+		String tumblrUrl = "http://liuhao2012.tumblr.com/page/";
+		for (int i = 40; i < 41; i++) {
+			System.out.println(i);
+			cte.updateEnex(importTumblr.getXmlDocument(tumblrUrl + String.valueOf(i)),
+				"TumblrPostExport.enex");
+		}
+	}
 }
--- src/main/java/SimpleBlog/plugin/MailToEvernote.java
+++ src/main/java/SimpleBlog/plugin/MailToEvernote.java
@@ -1,77 +1,113 @@
 package SimpleBlog.plugin;
 
-import SimpleBlog.entity.Blog;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import java.io.IOException;
+import java.util.Properties;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import java.util.Properties;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.sendgrid.*;
+
+import SimpleBlog.entity.Blog;
 
 /**
  * Post note to Evernote by sending email
  * Created by lyoo on 9/27/2015.
  */
 public class MailToEvernote {
-    private static final Logger logger = LogManager.getLogger(MailToEvernote.class.getName());
 
-    private String host;
-    private String port;
-    private String mailbox;
-    private String username;
+	private static final Logger logger = LogManager.getLogger(MailToEvernote.class.getName());
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	private String host;
+	private String port;
+	private String mailbox;
+	private String username;
+	private String password;
+	private String sendgridApiKey;
 
-    public void setHost(String host) {
-        this.host = host;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public void setPort(String port) {
-        this.port = port;
-    }
+	public void setHost(String host) {
+		this.host = host;
+	}
 
-    public void setMailbox(String mailbox) {
-        this.mailbox = mailbox;
-    }
+	public void setPort(String port) {
+		this.port = port;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public void setMailbox(String mailbox) {
+		this.mailbox = mailbox;
+	}
 
-    private String password;
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public boolean send(Blog blog) {
-        logger.entry();
-        System.setProperty("mail.mime.charset", "UTF-8");
-        Properties props = new Properties();
-        props.put("mail.smtp.host", host);
-        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.auth", true);
-        props.put("mail.smtp.port", port);
+	public void setSendgridApiKey(String sendgridApiKey) {
+		this.sendgridApiKey = sendgridApiKey;
+	}
 
-        Session session = Session.getInstance(props,
-                new javax.mail.Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
-                    }
-                }
-        );
+	public boolean send(Blog blog) {
+		logger.entry();
+		System.setProperty("mail.mime.charset", "UTF-8");
+		Properties props = new Properties();
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", true);
+		props.put("mail.smtp.port", port);
 
-        try {
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(username));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(mailbox));
-            message.setSubject(blog.getSubject() + " " + blog.getTags());
-            message.setText(blog.getContent());
-            Transport.send(message);
-        } catch (MessagingException e) {
-            logger.catching(e);
-            logger.error("Send mail to Evernote server failed:", e);
-            return logger.exit(false);
-        }
-        return logger.exit(true);
-    }
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(username));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(mailbox));
+			message.setSubject(blog.getSubject() + " " + blog.getTags());
+			message.setText(blog.getContent());
+			Transport.send(message);
+		} catch (MessagingException e) {
+			logger.catching(e);
+			logger.error("Send mail to Evernote server failed:", e);
+			return logger.exit(false);
+		}
+		return logger.exit(true);
+	}
+
+	public boolean sendByAPI(Blog blog) {
+		logger.entry();
+
+		Email from = new Email(username);
+		String subject = blog.getSubject() + " " + blog.getTags();
+		Email to = new Email(mailbox);
+		Content content = new Content("text/plain", blog.getContent());
+
+		Mail mail = new Mail(from, subject, to, content);
+		SendGrid sg = new SendGrid(sendgridApiKey);
+		Request request = new Request();
+		try {
+			request.method = Method.POST;
+			request.endpoint = "mail/send";
+			request.body = mail.build();
+			Response response = sg.api(request);
+			System.out.println(response.statusCode);
+			System.out.println(response.body);
+			System.out.println(response.headers);
+		} catch (IOException e) {
+			logger.catching(e);
+			logger.error("Sendgrid API send mail to Evernote server failed:", e);
+			return logger.exit(false);
+		}
+		return logger.exit(true);
+	}
 }
