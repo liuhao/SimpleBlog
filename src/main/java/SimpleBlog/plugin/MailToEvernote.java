ddd69217decbf1eb079d8a9de7d9e2c224d22123
Hao Liu <liu6d69@gmail.com>
2017-04-05 23:07:20 -0400

    Hide username and password fields.

--- src/main/java/SimpleBlog/plugin/MailToEvernote.java
+++ src/main/java/SimpleBlog/plugin/MailToEvernote.java
@@ -61,17 +61,18 @@ public class MailToEvernote {
 		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 		props.put("mail.smtp.auth", true);
 		props.put("mail.smtp.port", port);
-
+		String user = System.getenv(username);
+		String pwd = System.getenv(password);
 		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
 
 			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(username, password);
+				return new PasswordAuthentication(user, pwd);
 			}
 		});
 
 		try {
 			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(username));
+			message.setFrom(new InternetAddress(user));
 			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(mailbox));
 			message.setSubject(blog.getSubject() + " " + blog.getTags());
 			message.setText(blog.getContent());
@@ -87,7 +88,7 @@ public class MailToEvernote {
 	public boolean sendByAPI(Blog blog) {
 		logger.entry();
 
-		Email from = new Email(username);
+		Email from = new Email(System.getenv(username));
 		String subject = blog.getSubject() + " " + blog.getTags();
 		Email to = new Email(mailbox);
 		Content content = new Content("text/plain", blog.getContent());
